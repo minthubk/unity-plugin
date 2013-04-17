c28b3a030eea7564d5423e1c98e18cbae411a2d9
Mike <mike@prime31.com>
2013-04-16 17:56:07 -0700

    prep the Obj-C layer

---
+++ Appington/Assets/Plugins/iOS/AppingtonManager.h
@@ -0,0 +1,13 @@
+//
+//  AppingtonManager.h
+//  Unity-iPhone
+//
+//  Created by Mike Desaro on 4/16/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppingtonManager : NSObject
+
+@end
---
+++ Appington/Assets/Plugins/iOS/AppingtonManager.mm
@@ -0,0 +1,13 @@
+//
+//  AppingtonManager.m
+//  Unity-iPhone
+//
+//  Created by Mike Desaro on 4/16/13.
+//
+//
+
+#import "AppingtonManager.h"
+
+@implementation AppingtonManager
+
+@end
