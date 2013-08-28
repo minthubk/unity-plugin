513455d741faf319ba6a3f29dbb4805428ef5b94
Roger Binns <rogerb@rogerbinns.com>
2013-08-28 16:20:31 -0700

    Detect and be helpful on batchmode errors

--- build_plugin.sh
+++ build_plugin.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [ $# -ne 1 -o ! -d "$1" ]
 then
     echo "Supply the name of the output directory" >&2
@@ -9,3 +7,12 @@ then
 fi
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -quit -projectPath "`pwd`/Appington/" -exportPackage Assets "$1/AppingtonPlugin.unitypackage"
+
+res=$?
+
+if [ $res -ne 0 ]
+then
+    echo "Failed! $res"
+    echo "See ~/Library/Logs/Unity/Editor.log"
+    exit $res
+fi
