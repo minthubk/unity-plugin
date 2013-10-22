4ba296fbe72b1556970ffe2107fb288dff066e0f
Roger Binns <rogerb@rogerbinns.com>
2013-10-21 19:43:50 -0700

    Turn percentage into float for volume

--- Appington/Assets/Plugins/Appington/AppingtonManager.cs
+++ Appington/Assets/Plugins/Appington/AppingtonManager.cs
@@ -8,7 +8,7 @@ public class AppingtonManager : MonoBehaviour
 {
 	// Fired whenever an event from Appington is received
 	public static event Action<string,Dictionary<string,object>> onEvent;
-	
+
 	private float _initialVolume = 1;
 
 
@@ -18,8 +18,8 @@ public class AppingtonManager : MonoBehaviour
 		gameObject.name = this.GetType().ToString();
 		DontDestroyOnLoad( this );
 	}
-	
-	
+
+
 	void OnApplicationPause( bool didPause )
 	{
 		Appington.onApplicationPause( didPause );
@@ -32,14 +32,14 @@ public class AppingtonManager : MonoBehaviour
 		var dict = param.dictionaryFromJson();
 		var eventName = dict.ContainsKey( "name" ) ? dict["name"].ToString() : "Unknown";
 		var values = dict["values"] as Dictionary<string,object>;
-		
+
 		if( onEvent != null )
 			onEvent( eventName, values );
-		
+
 		handleEvent( eventName, values );
 	}
-	
-	
+
+
 	// Automatically handles ducking audio
 	private void handleEvent( string eventName, Dictionary<string,object> values )
 	{
@@ -49,7 +49,7 @@ public class AppingtonManager : MonoBehaviour
 			if( values.ContainsKey( "lowered_volume" ) )
 			{
 				_initialVolume = AudioListener.volume;
-				AudioListener.volume = float.Parse( values["lowered_volume"].ToString() );
+				AudioListener.volume = float.Parse( values["lowered_volume"].ToString() ) / 100.0f;
 			}
 		}
 		else if( eventName == "audio_end" )
@@ -61,4 +61,3 @@ public class AppingtonManager : MonoBehaviour
 
 
 }
-
